efa5995a0cee762ab8c3183d021cc6db7175a962
Roy R <tchalvakspam@gmail.com>
2019-07-01 19:43:46 -0400

    Comment out unused var.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import logo from './logo.svg'
+//import logo from './logo.svg'
 import './App.css'
 import Mockup from './components/Mockup/Mockup'
 
